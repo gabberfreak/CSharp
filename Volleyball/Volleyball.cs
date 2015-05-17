487d556d07095c48cf6e8b14e7c4cd07b7701338
gabbefreak <deliah@abv.bg>
2015-05-17 19:45:33 +0300

    commit

--- Volleyball/Volleyball.cs
+++ Volleyball/Volleyball.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Volleyball
+class Volleyball
 {
-    class Volleyball
+    static void Main()
     {
-        static void Main(string[] args)
+        string year = Console.ReadLine();
+        double holidays = double.Parse(Console.ReadLine());
+        double hometown = double.Parse(Console.ReadLine());
+
+        double playsInHometown = hometown;
+        double playsInHometownCounter = 48 - hometown;
+        double playsInNormalWeekend = playsInHometownCounter * 3 / 4;
+        double playInHolidays = holidays * 2 / 3;
+        double totalPlays = playsInHometown + playsInNormalWeekend + playInHolidays;
+
+
+        if (year == "leap")
         {
+            double leapPlays = totalPlays * 0.15;
+            Console.WriteLine(Math.Floor(totalPlays + leapPlays));
+        }
+        else
+        {
+            Console.WriteLine(Math.Floor(totalPlays));
         }
     }
 }
